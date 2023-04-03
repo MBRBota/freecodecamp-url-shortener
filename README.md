3c5ca6b30c390ce950fe021dbca65331d089f120
MBRBota <117132555+MBRBota@users.noreply.github.com>
2023-04-03 17:50:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# URL Shortener
+
+This is a URL shortener microservice project part of the freeCodeCamp 'Back End Development and APIs' certification process.
---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const port = 3000;
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(`${process.cwd()}/views/index.html`);
+});
+
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
