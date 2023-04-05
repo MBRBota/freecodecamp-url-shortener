476be91acce7ecf1a5830486828239f2e4627c5b
MBRBota <117132555+MBRBota@users.noreply.github.com>
2023-04-05 18:50:17 +0300

    feat: add post handler, url validation

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const dns = require('dns');
 const cors = require('cors');
 const app = express();
+const Url = require('./models/url');
 
-const port = 3000;
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
   res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
+app.post('/api/shorturl', (req, res) => {
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+  // per project requirements, only allow URLs with protocol intact
+  const protocolRegex = /^https?:\/\//ig;
+  const isValidUrl = protocolRegex.test(req.body.url);
+
+  // strip protocol from posted URL for validation
+  const hostname = req.body.url.replace(protocolRegex, '')
+
+  // URL validation
+  dns.lookup(hostname, (err, data) => {
+    if (err || !isValidUrl){
+      console.log(err);
+      console.log(isValidUrl)
+      res.status(400).json({ error: 'invalid url' })
+    } else {
+      res.json({ original_url: req.body.url })
+    }
+  })
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
---
+++ models/url.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema({
+    url: {
+        type: String,
+        required: true
+    }
+})
+
+const Url = mongoose.model('Url', urlSchema);
+module.exports = Url;
