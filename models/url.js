9875030e3f217012b32b393fa2c095c56e92e3c7
MBRBota <117132555+MBRBota@users.noreply.github.com>
2023-04-07 12:20:36 +0300

    feat: implement mongoose-sequence plugin

--- models/url.js
+++ models/url.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 const Schema = mongoose.Schema;
 
 const urlSchema = new Schema({
-    url: {
+    original_url: {
         type: String,
         required: true
     }
-})
+});
+
+urlSchema.plugin(AutoIncrement, { inc_field: 'short_url' });
 
 const Url = mongoose.model('Url', urlSchema);
 module.exports = Url;
